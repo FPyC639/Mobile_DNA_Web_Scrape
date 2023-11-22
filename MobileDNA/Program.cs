61b18bc73c00b141dcf81bc2fa0a5a6d0def9072
Jose M Serra Jr <joseserra803@gmail.com>
2023-11-22 16:35:39 -0500

    Merged titles and scraped authors from multiple websites.

--- MobileDNA/Program.cs
+++ MobileDNA/Program.cs
@@ -13,10 +13,8 @@ namespace MobileDNA
             var reader = new Process_HTML();
             Console.Write("Enter the Year: ");
             reader.year = Console.ReadLine();
-            var title_ls = reader.read_title();
-            title_ls.ForEach(x => Console.WriteLine(x));
             Console.WriteLine("Break\n\n\n");
-            reader.two_deep_extractor();
+            reader.write_html_csv();
             Console.ReadKey();
         }
     }
--- MobileDNA/Reader.cs
+++ MobileDNA/Reader.cs
@@ -9,15 +9,23 @@ using System.Net.Http;
 using System.Security.Policy;
 using System.Text;
 using System.Threading;
-
+/// <summary>
+/// Abstract class constructing Template to Process HTML for certain WebPage.
+/// </summary>
 public abstract class Reader_HTML
 {
-
+	abstract protected Uri creator(string url);
+	abstract protected Task<HtmlDocument> parse_webpage();
 	abstract public List<string> read_title();
-	
-	
-}
+    abstract public List<string> read_authors(HtmlDocument doc);
+    abstract public Task<List<List<string>>> two_deep_extractor();
 
+
+
+}
+/// <summary>
+/// Concrete class inheriting ReaderHTML abstract class that implements abstract methods for Mobile DNA website.
+/// </summary>
 public class Process_HTML : Reader_HTML
 {
 
@@ -36,7 +44,7 @@ public class Process_HTML : Reader_HTML
     /// <summary>
 	/// The purpose of this method is that it creates Uri's that can be used by the HTML Web Scraping libraries.
 	/// </summary>
-    private Uri creator(string url)
+    protected override Uri creator(string url)
 	{
 		
 		return new Uri(url);
@@ -46,7 +54,7 @@ public class Process_HTML : Reader_HTML
     /// <summary>
     /// The purpose of this method is that it parses the webpage and gathers the necessary components for individuals to collect data.
     /// </summary>
-    private async Task<HtmlDocument> parse_webpage()
+    protected override async Task<HtmlDocument> parse_webpage()
 	{
         var webGet = new HttpClient();
         this.query_string = this.url + string.Format(@"?query={0}&volume=&searchType=&tab=keyword", year);
@@ -82,21 +90,71 @@ public class Process_HTML : Reader_HTML
 		}        
 		return list;
     }
-
-	public async void two_deep_extractor()
+    public override List<string> read_authors(HtmlDocument doc)
+    {
+        var list = new List<string> ();
+        var author_name = doc.DocumentNode.SelectNodes(@"//a[@data-test='author-name']");
+        foreach( var author in author_name)
+        {
+            list.Add(author.InnerText);
+        }
+        return list;
+    }
+	public override async Task<List<List<string>>> two_deep_extractor()
 	{
+		var webpage_hrefs = new List<string>();
+        var webpage_authors = new List<List<string>>();
+        var webGet = new HttpClient();
         var doc = parse_webpage().Result;
         var a_href = doc.DocumentNode.SelectNodes(@"//h3//a[@itemprop='url']");
-		var csv = new StringBuilder();
-		string newLine = null;
-		int count = 0;
-		csv.AppendLine("Paper Index; Article_Link");
 		foreach( var a in a_href)
 		{
-			newLine = string.Format("{0};{1}", count, string.Concat(this.url.Replace("/articles",""),a.Attributes["href"].Value));
-			csv.AppendLine(newLine);
-			count++;
+			webpage_hrefs.Add(string.Concat(this.url.Replace("/articles",""),a.Attributes["href"].Value));
 		}
-		File.WriteAllText(@"C:\Users\joses\Source\Repos\Mobile_DNA_Web_Scrape\MobileDNA\Data2.txt", csv.ToString());
-	}
+		foreach( var str in webpage_hrefs)
+		{
+            var response = await webGet.GetAsync(creator(str));
+            if (response.IsSuccessStatusCode)
+            {
+                // Read the response content asynchronously 
+                response.Content.Headers.ContentType.CharSet = @"UTF-8";
+                string content = await response.Content.ReadAsStringAsync();
+
+
+                doc.LoadHtml(content);
+
+            }
+            else
+            {
+                Console.WriteLine($"Failed with status code: {response.StatusCode}");
+                doc = null;
+            }
+            var list = read_authors(doc);
+            webpage_authors.Add(list);
+        }
+        return webpage_authors;
+    }
+
+    public void write_html_csv()
+    {
+        var csv = new StringBuilder();
+        string newLine = null;
+        var titles = read_title();
+        var authors = two_deep_extractor().Result;
+        csv.AppendLine("Title;Authors");
+        List<string> mergedTitlesAndAuthors = new List<string>();
+        for (int i = 0; i < titles.Count; i++)
+        {
+            string title = titles[i];
+            List<string> authorList = authors[i];
+            string authorsString = string.Join(", ", authorList);
+            string mergedString = $"{title.Trim()}; {authorsString.Trim()}";
+            mergedTitlesAndAuthors.Add(mergedString);
+        }
+        foreach(string merger in mergedTitlesAndAuthors)
+        {
+            csv.AppendLine(merger);
+        }
+        File.WriteAllText(@"C:\Users\joses\Source\Repos\Mobile_DNA_Web_Scrape\MobileDNA\Data3.txt", csv.ToString());
+    }
 }
